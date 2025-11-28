3894fefebff710a3cebf6172d163037882619ba7
MONICA-R14 <moni142006@gmail.com>
2025-11-28 11:49:13 +0530

    Time: 20 ms (77.44%) | Memory: 23.6 MB (33.01%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p==root||q==root)
+        return root;
+
+        if((p->val > root->val && q->val < root->val)|| (p->val < root->val && q->val > root->val)) 
+        {
+            // both are in different subtrees so return root its only common ancestor bwt them 
+            return root;
+        }
+        else if(p->val > root->val && q->val > root->val)
+        {
+            //both in right subtree so again recurssive func to find lowest common ancestor but root=root->right bcz both p,q are in right subtree
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        else if(p->val < root->val && q->val < root->val){
+            return lowestCommonAncestor(root->left, p, q); // both are in left subtree
+         }
+         else 
+         return root;// this is not required. Adding it to avoid error like "missing return .."
+    }
+};
