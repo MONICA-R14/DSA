22e3ad73d0fc0a113d4dd82cfad2593935bbe627
MONICA-R14 <moni142006@gmail.com>
2026-01-01 22:40:06 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (57.46%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if(nums.size()==1)
+        
+        int ans = 0;
+        for (int x : nums) ans ^= x;//a ^ a = 0
+                                   //a ^ 0 = a this is xor property so a^a la all elements produce output as 0 so ans =0 then that one single element ^ 0(ans)=single element
+
+        return ans;
+        
+        
+        /*if(nums.size()==1)
         return nums[0];
 
        
@@ -15,5 +23,6 @@ public:
         }
        
         return nums[nums.size() - 1]; //last element must be the single one , since the above loop runs till n-1 th position
+        */
     }
 };
