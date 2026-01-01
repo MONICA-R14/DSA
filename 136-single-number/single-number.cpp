c94b08d2d317a29f52ee768c04c12c1a20c705bd
MONICA-R14 <moni142006@gmail.com>
2026-01-01 22:31:12 +0530

    Time: 14 ms (10.71%) | Memory: 20.7 MB (25.35%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size()==1)
+        return nums[0];
+
+       
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i=i+2)
+        {
+           
+            if(nums[i]!=nums[i+1])
+            return nums[i];
+           
+        }
+       
+        return nums[nums.size() - 1]; //last element must be the single one , since the above loop runs till n-1 th position
+    }
+};
