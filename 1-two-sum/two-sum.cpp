e70c458bd93c61df040c6c0df25aa7852e2cbf01
MONICA-R14 <moni142006@gmail.com>
2026-01-04 22:56:26 +0530

    Time: 3 ms (67.81%) | Memory: 14.9 MB (37.08%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,7 +2,20 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        for(int i=0;i<nums.size();i++)
+        unordered_map<int, int> pairIdx;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int num = nums[i];
+            if (pairIdx.find(target - num) != pairIdx.end()) {
+                return {i, pairIdx[target - num]};
+            }
+            pairIdx[num] = i;
+        }
+
+        return {}; 
+
+
+       /* for(int i=0;i<nums.size();i++)
         {
             int x=target-nums[i];
              
@@ -13,6 +26,6 @@ public:
                 return {i , int(it - nums.begin())}; //it-nums.begin() gives pointer , so to find the index of that pointer use int(that value) 
              }
         }
-        return {}; // if it fails
+        return {}; // if it fails*/
     }
 };
