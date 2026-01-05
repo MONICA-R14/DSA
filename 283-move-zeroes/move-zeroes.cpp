1f1df0e2b868d1c9c12846548af1cae5de9fa6fd
MONICA-R14 <moni142006@gmail.com>
2026-01-05 22:18:21 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (54.13%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left=0;
+           int j = 0;
+
+    // move all non-zero elements to the front
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != 0) {
+            nums[j++] = nums[i];
+        }
+    }
+
+    // fill remaining positions with zero
+    for (int i = j; i < nums.size(); i++) {
+        nums[i] = 0;
+    }
+
+       /* int left=0;
         for(int right=0;right<nums.size();right++)
         {
             if(nums[right]!=0)
@@ -9,6 +23,6 @@ public:
                 swap(nums[right],nums[left]);
                 left++;
             }
-        }
+        }*/
     }
 };
