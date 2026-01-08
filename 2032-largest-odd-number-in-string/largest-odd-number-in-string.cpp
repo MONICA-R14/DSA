16620ddf4fbefc6ff5facbd07666f1da54836655
MONICA-R14 <moni142006@gmail.com>
2026-01-08 21:35:43 +0530

    Time: 11 ms (5.24%) | Memory: 19.2 MB (7.89%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        string fire="";
+
+        for(int i=num.length()-1; i>=0;i--)
+        {
+            if((num[i] - '0')  % 2 != 0)  //num[i] -'0' gives the char's integer value ----for string stoi(string);
+            {
+                for(int k=0;k<=i;k++)
+                {
+                    fire+=num[k];
+                
+                }
+              // replace for loop with---- return num.substr(0, i + 1);
+
+                break;
+            }
+        }
+        return fire;
+    }
+};
