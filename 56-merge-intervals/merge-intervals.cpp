4aff51c4850eaeb77689571865d4e57e9c3e8ad8
MONICA-R14 <moni142006@gmail.com>
2026-02-22 14:16:12 +0530

    Time: 6 ms (46.09%) | Memory: 23.9 MB (58.90%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,6 +2,22 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& nums) {
         sort(nums.begin(),nums.end());
+        vector<vector<int>>res;
+        res.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++)
+        {
+            if(res.back()[1] >= nums[i][0])
+            {
+                res.back()[1]=max(res.back()[1],nums[i][1]);
+            }
+            else{
+                res.push_back(nums[i]);
+            }
+        }
+        return res;
+
+
+        /*sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-1;)
         {
             if(nums[i][1]>=nums[i+1][0])
@@ -12,6 +28,6 @@ public:
             //dont increment i bcz next time this new nums[i][1] only needs to be checked with nums[i+2][1] (since nums[i+1] is deleted)   
             else{ i++; }   
          }
-         return nums;
+         return nums;*/
     }
 };
