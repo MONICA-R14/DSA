6265e9250dbd5df3119a0ad1ecbba14886c03066
MONICA-R14 <moni142006@gmail.com>
2026-02-24 18:58:59 +0530

    Time: 4 ms (31.88%) | Memory: 10.9 MB (13.80%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-     vector<int>v;
-     if(x<0)return false;
-     while(x!=0)
-     {
-        v.push_back(x%10);
-        x=x/10;
-     }
-     int n=v.size();
-     for(int i=0;i<n/2;i++)
-     {
-        if(v[i]!=v[n-i-1])
+      string s=to_string(x);
+      int n=s.length();
+      int l=0,r=n-1;
+      for(int i=0;i<n/2;i++)
+      {
+        if(s[i]!=s[n-i-1])
         return false;
-     }
+      }
+
      return true;
      
       /*long long sum=0,rem=0;
