b6dbe84d5aef8a79e09323c115441395e1e7ea63
MONICA-R14 <moni142006@gmail.com>
2026-02-24 19:06:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (65.53%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-      string s=to_string(x);
+      if(x<0 || x%10==0 && x!=0) return false;
+      int rev=0;
+       //https://chatgpt.com/share/699da8ec-dd14-800b-a8e0-353e46680ebb
+      while(x>rev)//We only want to reverse HALF. so x>rev not like x>0 noo
+      {
+        rev=rev*10 + x%10; // to maintain ones tens place (rev*10) , x%10 to get last digit
+        x/=10;
+      }
+      return (x==rev || x==rev/10);// x=rev/10 It comes when the number has odd number of digits.
+
+      /*string s=to_string(x);
       int n=s.length();
       int l=0,r=n-1;
       for(int i=0;i<n/2;i++)
@@ -10,7 +20,7 @@ public:
         return false;
       }
 
-     return true;
+     return true;*/
      
       /*long long sum=0,rem=0;
       int n=x;
