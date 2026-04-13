259b894e4bcd9a0cbaefc6effef6f1cfb7cb7463
MONICA-R14 <2024503510@student.annauniv.edu>
2026-04-13 22:21:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.20%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+   // Tabulation 
+        int n=nums.size();
+        vector<int> dp(n, 0);
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            int take = nums[i] + (i >= 2 ? dp[i-2] : 0);
+            int nottake = dp[i-1];
+            dp[i] = max(take, nottake);
+        }
+        return dp[n-1];
+    }
+   /* int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
         return  f(0,nums,dp);
@@ -17,5 +29,5 @@ public:
         return dp[ind]=max(take,nottake);
 
 
-    }
+    }*/
 };
