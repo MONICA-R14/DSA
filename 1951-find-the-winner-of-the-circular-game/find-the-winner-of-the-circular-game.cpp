4c0c226e20932d2e6e1d0938fe8d93f1f512fc60
MONICA-R14 <2024503510@student.annauniv.edu>
2026-05-24 19:41:56 +0530

    Time: 11 ms (30.04%) | Memory: 28.1 MB (11.07%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+     vector<int>person;
+     int index=0;
+    int findTheWinner(int n, int k ) {
+        for(int i=0;i<n;i++)
+        {
+            person.push_back(i);
+        }
+        return recurssion(person,n,k);
+    }
+    int recurssion(vector<int>person,int n,int k)
+    {
+        if(n==1)
+        return person[0]+1;
+        index=(index+k-1)%n;
+        person.erase(person.begin() + index);
+        return recurssion(person,n-1,k);
+        
+    }
+};
