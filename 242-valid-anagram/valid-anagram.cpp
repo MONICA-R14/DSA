7c2b3538554b5d7a71a92c219f82b318be408589
MONICA-R14 <2024503510@student.annauniv.edu>
2026-05-26 13:07:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (24.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,6 +2,24 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length()) return false;
+        unordered_map<char,int>freq;
+        for(char c:s)
+        {
+            freq[c]++;
+        }
+        for(char c:t)
+        {
+            freq[c]--;
+        }
+
+        for(auto i:freq)
+        {
+            if(i.second!=0)
+            return false;
+        }
+        return true;
+        /*
+        here we used 2 unorderedmaps , how abt one map? ref above solu
         unordered_map<char,int>freq1;
         unordered_map<char,int>freq2;
         for(int i=0;i<s.length();i++)
@@ -16,6 +34,6 @@ public:
             if(freq1[c]!=freq2[c])
             return false;
         }
-        return true;
+        return true;*/
     }
 };
