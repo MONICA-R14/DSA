42c91d35c433bae70001d98d0ca5a307e1541b53
MONICA-R14 <2024503510@student.annauniv.edu>
2026-05-26 13:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (76.43%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int>freq;
+      //https://chatgpt.com/share/6a1550e3-4d00-8324-9504-6fc964f8ae1f
+      char ans=0;
+
+      for(char c:s)
+      {
+        ans^=c;
+      }
+      for(char c:t)
+      {
+        ans^=c;
+      }
+      return ans;
+      
+       /* unordered_map<char,int>freq;
 
         for(char c:s)
         {
@@ -17,6 +30,6 @@ public:
             if(i.second==1||i.second==-1)
               return i.first;
         }
-        return ' ';
+        return ' ';*/
     }
 };
