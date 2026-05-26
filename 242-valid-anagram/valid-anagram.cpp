45602cd0ffda7b31f87f350f84b4b921586c173b
MONICA-R14 <2024503510@student.annauniv.edu>
2026-05-26 13:05:14 +0530

    Time: 7 ms (18.83%) | Memory: 10 MB (10.61%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        unordered_map<char,int>freq1;
+        unordered_map<char,int>freq2;
+        for(int i=0;i<s.length();i++)
+        {
+            freq1[s[i]]++;
+            freq2[t[i]]++;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s[i];
+            if(!freq2[c]) return false;
+            if(freq1[c]!=freq2[c])
+            return false;
+        }
+        return true;
+    }
+};
