e29faee1064becebeb694a6ecfa82b710d03eabc
MONICA-R14 <2024503510@student.annauniv.edu>
2026-05-31 13:59:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (59.21%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
+        //ref striver
+        int fact=1;
+        vector<int>numbers;
+        for(int i=1;i<n;i++)//to find fact of n-1
+        {
+            fact*=i;
+            numbers.push_back(i);//if n=3 then numbers=[1,2,3(pushed after loop below)] at end of loop
+        }
+        numbers.push_back(n);//[1,2,3]
+        string ans="";
+        k=k-1;//bcz we follow 0 indexing
+        while(true)
+        {
+            ans=ans + to_string(numbers[k/fact]);
+            numbers.erase(numbers.begin() + k/fact);
+            if(numbers.size()==0)
+             break;
+
+            k=k%fact;
+            fact=fact/numbers.size();
+        }
+
+        return ans;
+
+
+       /*// this is O(n!*n) so dont use this
         //https://chatgpt.com/share/6a1bc823-e514-83ab-8c18-1baf0870ea73
         //ref note for understanding
         vector<int>path;
@@ -14,6 +40,7 @@ public:
     void func(int n,vector<int>& path,vector<int>& vis,int& count,int k,string& ans)
     {
         //base case
+        if(!ans.empty()) return; //here already ans found so no need to check base case and all just return ,no need further proceedings
         if(path.size()==n)
         {
             count++;
@@ -40,5 +67,7 @@ public:
             vis[i]=0;//backtrack
         }
 
+    }
+    */
     }
 };
