de8ad33c4f4b0ec7aba506405a582532f64e25a4
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-01 19:26:24 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (29.38%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int r=matrix.size();
+        int c=matrix[0].size();
+
+        vector<int>row(r,0);
+        vector<int>clm(c,0);
+
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=1;
+                    clm[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<r;i++)
+        {
+             for(int j=0;j<c;j++)
+             {
+                if(row[i] || clm[j])
+                {
+                    matrix[i][j]=0;
+                }
+             }
+           
+        }
+    }
+};
