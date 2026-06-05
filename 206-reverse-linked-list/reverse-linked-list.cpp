b7f8dd68167f1a5810c68fde23a07efb7556cb90
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-05 10:51:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (71.14%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -14,6 +14,22 @@ public:
         ListNode* next=nullptr;
         ListNode* prev=nullptr;
         ListNode* curr=head;
+
+        while(curr!=nullptr)
+        {
+            next=curr->next;
+            curr->next=prev;
+
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+        
+        
+        
+        /*ListNode* next=nullptr;
+        ListNode* prev=nullptr;
+        ListNode* curr=head;
         while(curr!=nullptr)
         {
             next=curr->next;
@@ -22,5 +38,6 @@ public:
             curr=next;
         }
         return prev;
+        */
     }
 };
