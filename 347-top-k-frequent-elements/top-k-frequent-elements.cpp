699ccd4c2182d8e6a839e02405f119a9b39fffeb
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-06 13:25:32 +0530

    Time: 7 ms (15.73%) | Memory: 18 MB (39.52%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -10,11 +10,13 @@ public:
         vector<pair<int,int>>fr(freq.begin(),freq.end());
         // sort(fr.begin(),fr.end()); tihs sorts by key not value 
         //below one sorts by value so correct
-         sort(fr.begin(), fr.end(),
-             [](auto &a, auto &b)
-             {
+        //https://chatgpt.com/s/t_6a23d1e4db7c8191b4734d57127edaea
+         sort(fr.begin(), fr.end(), [](auto &a, auto &b)   {
                  return a.second > b.second;
              });
+              //generally return_type func(para) { return x; } 
+             // [] is to catch returned value , then no func name , (para passed as reference & )
+             //inside { return bigger value ;} 
 
         for(int i=0;i<k;i++)
         {
