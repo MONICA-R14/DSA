19e41ee2093911537784a09dde85ec356d1f1419
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-06 09:12:54 +0530

    Time: 7 ms (41.17%) | Memory: 26.4 MB (14.36%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -8,27 +8,14 @@ public:
             ans.push_back(nums[0]);
             return ans;
         }
-        
+        int y=n/3;
         for(int i=0;i<n;i++)
         {
             freq[nums[i]]++;
-        }
-        int y=n/3;
-        //this is wrong dont do this ,dont iterate through nums if u do so then duplicate elements appear twice so no! ,(eg: in eg1 , o/p will be [3,3] which is completely wrong so no!!)
-        /*for(int i=0;i<n;i++)
-        {
-            if(freq[nums[i]]>y)
-            {
+            if(freq[nums[i]]==y+1) 
                 ans.push_back(nums[i]);
-            }
-        }*/
-        for(auto &it : freq)
-        {
-            if(it.second > y)
-            {
-                ans.push_back(it.first);
-            }
         }
+       
         return ans;
     }
 };
