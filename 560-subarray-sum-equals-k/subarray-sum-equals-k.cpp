b451413fa9bef48cf64a9e925fb39138682a227b
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-08 10:16:31 +0530

    Time: 43 ms (57.32%) | Memory: 45.4 MB (56.20%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int>freq;
-        freq[0]=1;
-        int presum=0,count=0;
-        for(int i=0;i<nums.size();i++)
+        //https://chatgpt.com/s/t_6a26482bc9cc8191bc82b32919adea0f
+      //https://chatgpt.com/s/t_6a2647d60d9481918b0ccba8648cef3a
+      //ref strivers second solu : prefix+hashmap , bcz 3rd solu is only for +ve elements but here we have -ve elements too so dont use that 3rd
+
+      int prefix=0;
+      int count=0;
+      unordered_map<int,int>mp;
+      mp[0]=1; //let 0 be there once (freq = 1) for zero
+      for(int num : nums)
+      {
+        prefix+=num;
+
+        if(mp.find(prefix-k)!=mp.end())
         {
-            presum+=nums[i];
-             if(freq.find(presum - k) != freq.end())
-            count += freq[presum - k];
-            /*int remove=presum-k;
-            count+=freq[remove];*/
-            freq[presum]+=1;
+            count+=mp[prefix-k]; //ref why in second link of chatgpt , add freqcount , bcz particular sum  may occur manytime
         }
-        return count;
+        mp[prefix]++;
+      }
+      return count;
     }
 };
