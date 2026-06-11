c7028aefb2b5a7af8f33dff6a1ccbd47a12de73c
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-11 21:11:52 +0530

    Time: 5 ms (69.85%) | Memory: 39.4 MB (32.87%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -29,14 +29,20 @@ public:
                 int newr=row+r[i];
                 int newc=clm+c[i];
 
-               
+                if(newr< 0 || newr>=n || newc <0 || newc>=m)
+                {
+                    if(!(row==entrance[0] && clm==entrance[1])) //here we need to check row and clm bcz from that place grid only we exit 
+                    {
+                        return p;
+                    }
+                }
                 if(newr >= 0&& newr<n && newc<m && newc>=0 && maze[newr][newc]=='.' && !visited[newr][newc] )
                 {
                     //check if its near exit , exit ={row <0 or =n or clm <0 or =m }and not !=entrance
-                    if((newr==0 || newr==n-1 || newc==0 || newc==m-1) && !(newr==entrance[0] && newc==entrance[1]))
+                    /*if((newr==0 || newr==n-1 || newc==0 || newc==m-1) && !(newr==entrance[0] && newc==entrance[1]))
                     {
                      return p+1;
-                    }
+                    }*/
                     visited[newr][newc]=1;
                     q.push({{newr,newc},p+1});
 
