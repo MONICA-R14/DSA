c891f3c7ef5bf11e3fc6a907b584108d39cbb7cb
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-11 20:58:54 +0530

    Time: 6 ms (67.09%) | Memory: 39.2 MB (36.72%) - LeetSync

---
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        //apply bfs
+        int n=maze.size();
+        int m=maze[0].size();
+        
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
+        q.push({{entrance[0],entrance[1]},0}); // 0-for path length
+        
+        int r[]={-1,0,1,0};
+        int c[]={0,1,0,-1};
+        visited[entrance[0]][entrance[1]]=1;
+
+        while(!q.empty())
+        {
+            
+            int row=q.front().first.first;
+            int clm=q.front().first.second;
+            
+            int p=q.front().second;
+            q.pop();
+
+            
+    
+            for(int i=0;i<4;i++)
+            {
+                int newr=row+r[i];
+                int newc=clm+c[i];
+
+               
+                if(newr >= 0&& newr<n && newc<m && newc>=0 && maze[newr][newc]=='.' && !visited[newr][newc] )
+                {
+                    //check if its near exit , exit ={row <0 or =n or clm <0 or =m }and not !=entrance
+                    if((newr==0 || newr==n-1 || newc==0 || newc==m-1) && !(newr==entrance[0] && newc==entrance[1]))
+                    {
+                     return p+1;
+                    }
+                    visited[newr][newc]=1;
+                    q.push({{newr,newc},p+1});
+
+                }
+                
+                
+            }
+        }
+       return -1; 
+    }
+   
+};
