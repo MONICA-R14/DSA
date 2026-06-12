1def384e94608aa597fdf68a69304f507682ac4d
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-12 09:45:21 +0530

    Time: 12 ms (14.68%) | Memory: 19.7 MB (42.67%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& prerequisites) {
+        // Code here
+ // https://youtu.be/73sneFXuTEg?si=-XVUDHwNwUu7w2d6  -- topological sort / khan algo
+ // https://youtu.be/iTBaI90lpDQ?si=Y59iGUV_OLW7kH16   --detect cycle in graph using khan algo
+ // https://www.youtube.com/watch?v=WAOfKpxYHR8&t=440s
+ 
+        vector<int>adj[V];
+        
+        //if [1,0] then 0-->1 , 0 should happen before 1 so, 0-->1
+        // [2,1] then 0-->1-->2 is correct , 0 before 1 , 1 before 2 
+        
+        //but striver used [1,0] , 1-->0 , check which is correct
+        //both will give ans for this question 
+      
+
+        for(auto it:prerequisites)
+        {
+            adj[it[0]].push_back(it[1]);
+        }
+        
+        vector<int>indegree(V,0);
+        //calculate indegree for all nodes
+        for(int i=0;i<V;i++)
+        {
+            for(auto it:adj[i])//for each node mark neighbour's indegree++
+            {
+                indegree[it]++;
+            }
+        }
+        //if indegree is 0 insert into queue
+        queue<int>q;  
+        for(int i=0;i<V;i++)
+        {
+           if(indegree[i]==0)
+           {
+               q.push(i);
+           }
+        }
+        vector<int>topo;  //to store topo order
+        while(!q.empty())
+        {
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                 q.push(it);
+            }
+        }
+        
+        return topo.size()==V;
+    }
+};
