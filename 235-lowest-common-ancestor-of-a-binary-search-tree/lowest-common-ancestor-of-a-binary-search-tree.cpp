cfef8d7a35ab17b3610569c40704d23bee839847
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-17 10:19:26 +0530

    Time: 24 ms (49.24%) | Memory: 23.6 MB (34.22%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,7 +11,30 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(p==root||q==root)
+         
+         if(!root) return root;
+         if(p==nullptr || q==nullptr) return root;
+
+         if(p->val >= root->val && q->val <= root->val) 
+           return root; 
+          if(p->val <= root->val && q->val >= root->val) 
+           return root; 
+
+          if(p->val > root->val && q->val >root->val)
+           root= lowestCommonAncestor(root->right,p,q);
+
+           else 
+             root=lowestCommonAncestor(root->left,p,q);
+
+             return root;
+        
+        
+        
+        
+        
+        
+        
+        /*if(p==root||q==root)
         return root;
 
         if((p->val > root->val && q->val < root->val)|| (p->val < root->val && q->val > root->val)) 
@@ -29,5 +52,6 @@ public:
          }
          else 
          return root;// this is not required. Adding it to avoid error like "missing return .."
+         */
     }
 };
