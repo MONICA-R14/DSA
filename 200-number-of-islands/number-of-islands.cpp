3da0507d2cda4e4db9ef75b39a83853fb152973b
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-17 12:09:27 +0530

    Time: 29 ms (41.67%) | Memory: 17.5 MB (42.92%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,9 +1,47 @@
 class Solution {
 public:
+  int n=0;
+  int m=0;
     int numIslands(vector<vector<char>>& grid) {
         //refer takeuforward video
+        //When the graph is given as vertices and edges , find adj list else no need to find
+        //here adj matrix given so no need to find adj list 
+         n=grid.size();  m=grid[0].size();
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1' && !visited[i][j])
+                {
+                    dfs(i,j,grid,visited);
+                    count++;
+                }
+            }
+        }
+        return count;  
+    }
+    void dfs(int row,int clm,vector<vector<char>>& grid,vector<vector<int>>& visited)
+    {
+      visited[row][clm]=1;
+       int r[]={-1,0,1,0}; 
+       int c[]={0,1,0,-1};
 
-        int n=grid.size();
+       for(int i=0;i<4;i++)
+       {
+        int newr=row+r[i];
+        int newc=clm+c[i];
+
+        if(newr >=0 && newr < n && newc >=0 && newc < m && grid[newr][newc]=='1' && !visited[newr][newc])
+        {
+            dfs(newr,newc,grid,visited);
+        }
+       }
+    }
+};
+/*
+ int n=grid.size();
         int m=grid[0].size();
         int c=0;
         vector<vector<int>>visited(n,vector<int>(m,0));
@@ -14,7 +52,7 @@ public:
             {
                 if(grid[i][j]=='1' && !visited[i][j] )
                 {
-                    dfs(i,j,n,m,visited,grid);
+                    dfs(i,j,n,m,visited,grid); // or use bfs , bfs code is written below dfs
                     c++;
                 }
             }
@@ -42,7 +80,7 @@ public:
                      
        }
     }
-    /*void bfs(int r,int c,int n,int m,vector<vector<int>>& visited,vector<vector<char>>& grid)
+    void bfs(int r,int c,int n,int m,vector<vector<int>>& visited,vector<vector<char>>& grid)
     {
         queue<pair<int,int>>q;
         q.push({r,c});
@@ -72,5 +110,3 @@ public:
             }
         }
         */
-    
-};
