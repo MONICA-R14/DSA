3a79b4beb4c8244e092869f76db4b54473563381
MONICA-R14 <2024503510@student.annauniv.edu>
2026-06-28 10:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (71.07%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-       stack<char>st;
+      
+      string ans;
+
+      for(int i=0;i<s.size();i++)
+      {
+        if(!ans.empty() && s[i]==ans.back())
+        {
+            ans.pop_back();
+        }
+        else ans.push_back(s[i]);
+      }
+
+      return ans;
+      
+      /* stack<char>st;
        string ans="";
        for(int i=0;i<s.length();i++)
        {
@@ -18,6 +32,6 @@ public:
         st.pop();
        }
         reverse(ans.begin(),ans.end());
-       return ans;
+       return ans; */
     }
 };
