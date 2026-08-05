ca16d761e731830d10478bb95b62912e75bc6b0f
MONICA-R14 <2024503510@student.annauniv.edu>
2026-08-05 21:13:27 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (42.33%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        //ref grey note front side from interview page onwards
+       unordered_map<int,int>freq;
+
+       for(int i=0;i<nums.size();i++)
+       {
+        freq[nums[i]]++;
+       }
+       //min heap
+       priority_queue<pair<int,int> ,
+                      vector<pair<int,int>>,
+                      greater<pair<int,int>>>pq;
+        for(auto& p : freq)
+        {
+            pq.push({p.second , p.first}); //insert frequency first bcz based on second value only we alter pq accordinlgy
+
+            if(pq.size()>k)
+            pq.pop();
+        }
+
+        vector<int>ans;
+
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+       /*
+        // ref better solu https://chatgpt.com/share/6a23d2df-29e8-8322-b395-8710ba4fcd5b
+        //or below one but its not optimal
         unordered_map<int,int>freq;
         vector<int>ans;
         for(int i=0;i<nums.size();i++)
@@ -23,5 +53,6 @@ public:
             ans.push_back(fr[i].first);
         }
         return ans;
+        */
     }
 };
